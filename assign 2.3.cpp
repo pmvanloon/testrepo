7d36660a296030a162a1349ed034262f60f2cc96
pmvanloon <34141761+pmvanloon@users.noreply.github.com>
2020-09-16 17:38:34 -0700

    Add files via upload

---
+++ assign 2.3.cpp
@@ -0,0 +1,13 @@
+// This program demonstrates the string fast.
+#include <iostream>
+#include <string> // Required for string class.
+using namespace std;
+
+int main ()
+{
+    string movietitle;
+
+    movietitle = "wheels of fury";
+    cout << "My favorite movie is " << movietitle << endl;
+    return 0;
+}
